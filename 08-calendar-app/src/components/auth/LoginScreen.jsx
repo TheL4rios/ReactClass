dbf105b00ca28c115865700dde4b0ce379ac05eb
TheL4rios <arallariosqu@ittepic.edu.mx>
2021-06-27 22:54:44 -0500

    calendar app

--- 08-calendar-app/src/components/auth/LoginScreen.jsx
+++ 08-calendar-app/src/components/auth/LoginScreen.jsx
@@ -10,15 +10,15 @@ export const LoginScreen = () => {
     const dispatch = useDispatch();
 
     const [formLoginValues, handleLoginInputChange] = useForm({
-        lEmail: 'arallariosqu@ittepic.edu.mx',
-        lPassword: '123456'
+        lEmail: '',
+        lPassword: ''
     });
 
     const [formRegisterValues, handleRegisterInputChange] = useForm({
-        rName: 'Larios',
-        rEmail: 'arallariosqu@ittepic.edu.mx',
-        rPassword1: '123456',
-        rPassword2: '123456'
+        rName: '',
+        rEmail: '',
+        rPassword1: '',
+        rPassword2: ''
     });
 
     const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues;
